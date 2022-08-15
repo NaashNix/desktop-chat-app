f5c5d7f6020e23126de0d1bfe5ef36622dc7c6e6
NaashNix <thilakshanaravindu@gmail.com>
2022-08-15 22:23:35 +0530

    Emoji sending done

--- src/controllers/ClientFormController.java
+++ src/controllers/ClientFormController.java
@@ -4,17 +4,24 @@ import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
+import javafx.event.EventType;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.URL;
+import java.util.EventListener;
 import java.util.ResourceBundle;
 
 public class ClientFormController implements Initializable {
@@ -25,10 +32,35 @@ public class ClientFormController implements Initializable {
     public Label usernameLabel;
     public VBox vb_main;
     public AnchorPane clientContext;
+    public ImageView emojiIcon;
+    public ScrollPane sp_emoji;
+    public GridPane gp_emoji;
+    public FlowPane as;
     private Client client;
+    int[] emojis = {
+            0x1F606,
+            0x1F601,
+            0x1F602,
+            0x1F609,
+            0x1F618,
+            0x1F610,
+            0x1F914,
+            0x1F642,
+            0x1F635,
+            0x1F696,
+            0x1F636,
+            0x1F980,
+            0x1F625,
+            0x1F634,
+            0x1F641,
+            0x1F643,
+    };
 
     public void sendOnAction(ActionEvent actionEvent) {
+        sp_emoji.setVisible(false);
         String message = messageTextField.getText();
+        message = message.replace(":)", new String(Character.toChars(0x1F606)));
+
         if (!message.isEmpty()) {
             sendMessage(message);
             messageTextField.clear();
@@ -39,6 +71,12 @@ public class ClientFormController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        setEmojisToPane();
+
+        sp_emoji.setVisible(false);
+        sp_emoji.setStyle("-fx-background-color: coral;");
+
         if (username != null){
             usernameLabel.setText(username);
         }else {
@@ -82,6 +120,37 @@ public class ClientFormController implements Initializable {
         messageLbl.setStyle("-fx-background-color:  #3498db;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
         hBox.getChildren().add(messageLbl);
         vb_main.getChildren().add(hBox);
+
+    }
+
+    public void emojiOnClick(MouseEvent mouseEvent) {
+        if (sp_emoji.isVisible()){
+            sp_emoji.setVisible(false);
+        }else {
+            sp_emoji.setVisible(true);
+            Text text = new Text(new String(Character.toChars(emojis[4])));
+            text.setStyle("-fx-font-size: 25px; -fx-font-family: 'Noto Emoji';");
+            gp_emoji.add(text, 0, 0);
+        }
+    }
+
+    private void setEmojisToPane() {
+        int EMOJI_INDEX = 0;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 4; i++) {
+                Label text = new Label(new String(Character.toChars(emojis[EMOJI_INDEX++])));
+                text.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                    @Override
+                    public void handle(MouseEvent event) {
+                        messageTextField.appendText(text.getText());
+                    }
+                });
+                text.setStyle("-fx-font-size: 25px;" +
+                        " -fx-font-family: 'Noto Emoji';" +
+                        "-fx-text-alignment: center;");
+                gp_emoji.add(text, i, j);
+            }
+        }
     }
 
 }
